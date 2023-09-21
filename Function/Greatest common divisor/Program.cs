37f8a2c7cfb254d07b121b874eded8996b86cc5d
BaskakovAnton <8Bazz8@mail.ru>
2023-09-21 14:42:47 +0300

    edit Function/Greatest common divisor/Program.cs~

--- Function/Greatest common divisor/Program.cs
+++ Function/Greatest common divisor/Program.cs
@@ -1,2 +1,19 @@
 ﻿/* Наибольший общий делитель (НОД):
 Напишите функцию, которая принимает два целых числа в качестве параметров и возвращает их наибольший общий делитель (НОД). */
+int FindGCD(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+System.Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число: ");
+int number2 = int.Parse(Console.ReadLine());
+int gcd = FindGCD(number1, number2);
+Console.WriteLine($"НОД чисел {number1} и {number2} равен {gcd}");
