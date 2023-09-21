8715f5bbd01e10f5b2876cc1e442563bdefd35a1
BaskakovAnton <8Bazz8@mail.ru>
2023-09-21 15:34:38 +0300

    edit Function/Maximum element/Program.cs

--- Function/Maximum element/Program.cs
+++ Function/Maximum element/Program.cs
@@ -1,2 +1,26 @@
-﻿/* Поиск максимального элемента:
+﻿using System;
+/* Поиск максимального элемента:
 Напишите функцию, которая находит и возвращает максимальный элемент в заданном массиве целых чисел. */
+int MaxElement(int[] Array)
+{
+    int length = Array.Length;
+    int max = Array[0];
+    int i = 1;
+    while (i < length)
+    {
+        if (max < Array[i])
+        {
+            max = Array[i];
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return max;
+}
+
+int[] Arr = { 99, 12, 31 };
+int result = MaxElement(Arr);
+Console.WriteLine(result);
