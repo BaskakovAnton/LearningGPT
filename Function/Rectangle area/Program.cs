a2b748008a16c0ee881791e26b8d3f479a8200f0
BaskakovAnton <8Bazz8@mail.ru>
2023-09-21 14:17:35 +0300

    edit Function/Rectangle area/Program.cs

--- Function/Rectangle area/Program.cs
+++ Function/Rectangle area/Program.cs
@@ -1,2 +1,16 @@
 ﻿/* Площадь прямоугольника:
 Напишите функцию, которая принимает ширину и высоту прямоугольника в качестве параметров и возвращает его площадь. */
+
+int RectangleArea(int arg1, int arg2)
+{
+    int result = arg1 * arg2;
+    return result;
+}
+
+System.Console.WriteLine("Введите длину первой стороны: ");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите длину второй стороны: ");
+int b = int.Parse(Console.ReadLine());
+
+int RecArea = RectangleArea(a, b);
+System.Console.WriteLine($"Площадь прямоугольника равна = {RecArea}");
