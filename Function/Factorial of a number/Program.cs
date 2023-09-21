42485951292daad90ff38ec1c146217f62454f6e
BaskakovAnton <8Bazz8@mail.ru>
2023-09-21 14:32:31 +0300

    edit Function/Factorial of a number/Program.cs01~

--- Function/Factorial of a number/Program.cs
+++ Function/Factorial of a number/Program.cs
@@ -1,2 +1,19 @@
 ﻿/* Факториал числа:
 Напишите функцию, которая принимает целое положительное число в качестве параметра и возвращает его факториал (произведение всех целых чисел от 1 до этого числа). */
+int Factorial(int arg1)
+{
+    int result = 1;
+    int i = 1;
+    while (i<= arg1)
+    {
+        result = result * i;
+        i++;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int fac = Factorial(a);
+System.Console.WriteLine(fac);
